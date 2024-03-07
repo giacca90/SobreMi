863201534159da70723ef5ab46ce3261efeb7b05
Giacca90 <giacca90mobile@gmail.com>
2024-03-07 23:16:09 +0100

    prueba3

--- index.ts
+++ index.ts
@@ -9,22 +9,6 @@ async function obtenerCertificados() {
     listaArchivos = await response.json();
 }
 
-
-/* const importAll = (r: any) => r.keys().map(r);
-
-const context = (require as any).context('../certificados/', false, /\.pdf$/);
-const certificados:string[] = importAll(context);
-certificados.forEach((certificado: string, index: number) => {
-    const embedElement = document.createElement('embed');
-    embedElement.src = certificado;
-    embedElement.type = 'application/pdf';
-    embedElement.width = '500';
-    embedElement.height = '600';
-    embedElement.title = `Certificado ${index + 1}`;
-
-    resultado.appendChild(embedElement);
-}); */
-
 function buscar(valor:string) {
     
 }
